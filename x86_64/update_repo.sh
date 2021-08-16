0f0dce867888e487c69f1aa65b7d722c313d5deb
Aranya Das <iamaranya@hotmail.com>
2021-08-16 13:59:17 +0530

    update

---
+++ x86_64/update_repo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm tdlinux-repo*
+
+echo "repo-add"
+repo-add -s -n -R tdlinux-repo.db.tar.gz *.pkg.tar.zst
+sleep 5
+
+echo "####################################"
+echo "Repo Updated!!"
+echo "####################################"
+
